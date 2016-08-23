0e18303d3df337870163ba8c174e4535708e2d21
qzed <qzed@users.noreply.github.com>
2016-08-23 04:49:12 +0200

    add comments for BuildSetup

--- microtrafficsim-core/src/main/java/microtrafficsim/build/BuildSetup.java
+++ microtrafficsim-core/src/main/java/microtrafficsim/build/BuildSetup.java
@@ -1,6 +1,15 @@
 package microtrafficsim.build;
 
 
+/**
+ * Debug switches.
+ *
+ * @author Maximilian Luz
+ */
 public class BuildSetup {
+
+    /**
+     * Enable/disable debug instructions for the visualization.
+     */
     public static final boolean DEBUG_CORE_VIS = true;
 }
