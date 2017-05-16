f214b4513c8604b1349ca61f15e45447741b0d5f
qzed <qzed@users.noreply.github.com>
2017-05-16 19:08:53 +0200

    Port osm_get_xml.py to Python 3

--- scripts/osm_get_xml.py
+++ scripts/osm_get_xml.py
@@ -2,21 +2,20 @@
 """
 OpenStreetMap XML file downloader.
 
-Requires: urllib2, docopt
-Not testet with python 3 yet.
+Requires python 3, urllib and docopt
 
 Note: The download-size is limited, for larger areas refer to
 'http://download.geofabrik.de/'
 """
 __author__ = "Maximilian Luz"
 
-from docopt import docopt
-import urllib2
 import os
+import urllib3
+from docopt import docopt
 
 
 # Note: openstreetmap.org seems unable to handle large areas, thus use overpass
-usage = """OpenStreetMap XML downloader.
+USAGE = """OpenStreetMap XML downloader.
 
 Usage:
   osm_get_xml.py [options]
@@ -42,44 +41,26 @@ def osm_get_region_xml(api_url, bb_left, bb_bottom, bb_right, bb_top, filename):
     url = api_url + "/map?bbox={0},{1},{2},{3}"
     url = url.format(str(bb_left), str(bb_bottom), str(bb_right), str(bb_top))
 
-    # open file
-    f = open(filename, 'wb')
-
-    print("downloading '{0}' to '{1}'".format(url, filename))
-
-    error = False
+    # open file and connection
+    http = urllib3.PoolManager()
+    has_failed = False
     try:
-        # open connection
-        remote = urllib2.urlopen(url)
-        buffer_size = 8129
+        print("downloading '{0}' to '{1}'".format(url, filename))
+        with open(filename, "wb") as target:
+            remote = http.request('GET', url)
+            target.write(remote.data)
 
-        # download and write file
-        buffer = remote.read(buffer_size)
-        while buffer:
-            f.write(buffer)
-            buffer = remote.read(buffer_size)
-
-    except urllib2.URLError as e:
-        error = True
-        if e.code == '400':
-            print("HTTP Error 400: 'Any of the node/way/relation limits are "
-                  + "crossed'")
-        elif e.code == '':
-            print("HTTP Error 509: 'You have downloaded too much data. "
-                  + "Please try again later'")
-        else:
-            print("HTTP Error {0}".format(e.code))
-
-    # close file, cleanup on error
-    f.close()
-    if error:
+    except Exception as err:
+        has_failed = True
         os.remove(filename)
-    else:
+        raise err
+
+    if not has_failed:
         print("download finished successfully!")
 
 
-if __name__ == '__main__':
-    args = docopt(usage, version='OSM XML downloader 1.0')
+def run(args):
+    """run the downloader with the specified arguments"""
     bounds = args['--bounds'].split(",")
     left = float(bounds[0].strip())
     bottom = float(bounds[1].strip())
@@ -87,3 +68,7 @@ if __name__ == '__main__':
     top = float(bounds[3].strip())
 
     osm_get_region_xml(args['--api'], left, bottom, right, top, args['--out'])
+
+
+if __name__ == '__main__':
+    run(docopt(USAGE, version='OSM XML downloader 1.0'))
