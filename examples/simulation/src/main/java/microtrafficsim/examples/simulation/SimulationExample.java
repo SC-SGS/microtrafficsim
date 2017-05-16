292790888d2327ee112c02f52d6b8d7225d80655
qzed <qzed@users.noreply.github.com>
2017-05-16 19:57:48 +0200

    SimulationExample: Fix window title

--- examples/simulation/src/main/java/microtrafficsim/examples/simulation/SimulationExample.java
+++ examples/simulation/src/main/java/microtrafficsim/examples/simulation/SimulationExample.java
@@ -536,7 +536,7 @@ public class SimulationExample {
      * @return the default frame title.
      */
     private String getDefaultFrameTitle() {
-        StringBuilder title = new StringBuilder("MicroTrafficSim - Map Viewer Example");
+        StringBuilder title = new StringBuilder("MicroTrafficSim - Simulation Example");
 
         if (file != null) {
             title.append(" - [").append(file).append("]");
