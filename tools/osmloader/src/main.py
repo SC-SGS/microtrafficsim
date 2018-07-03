1e04cb94c044f173fa21b8f093a1fad359a19429
dominicparga <dominic.parga@icloud.com>
2018-07-03 19:29:21 +0200

    Refactor a little

--- tools/osmloader/src/main.py
+++ tools/osmloader/src/main.py
@@ -77,6 +77,7 @@ if __name__ == '__main__':
         metavar=('MAP_NAME'),
         type=str,
         nargs='+',
+        default=[],
         help=help
     )
 
@@ -88,6 +89,7 @@ if __name__ == '__main__':
         type=float,
         nargs=4,
         action='append',
+        default=[],
         help=help
     )
 
@@ -98,6 +100,7 @@ if __name__ == '__main__':
         metavar=('MAP_NAME'),
         type=str,
         nargs='+',
+        default=[],
         help=help
     )
 
@@ -110,28 +113,26 @@ if __name__ == '__main__':
     downloads = []
 
     # remember predefined maps and their related coordinates
-    if args.maps:
-        for map_name in args.maps:
-            try:
-                downloads.append((
-                    map_name,
-                    PREDEFINED[map_name]
-                ))
-            except KeyError:
-                err_msg = '\'' + map_name + '\''
-                err_msg += ' is expected to be predefined, but isn\'t.'
-                exit(err_msg)
+    for map_name in args.maps:
+        try:
+            downloads.append((
+                map_name,
+                PREDEFINED[map_name]
+            ))
+        except KeyError:
+            err_msg = '\'' + map_name + '\''
+            err_msg += ' is expected to be predefined, but isn\'t.'
+            exit(err_msg)
 
     # remember custom maps and their related coordinates
-    if args.out and args.bounds:
-        if len(args.out) > len(args.bounds):
-            err_msg = 'There are more custom map names than bounds.'
-            exit(err_msg)
-        if len(args.out) < len(args.bounds):
-            err_msg = 'There are more bounds than custom map names.'
-            exit(err_msg)
+    if len(args.out) > len(args.bounds):
+        err_msg = 'There are more custom map names than bounds.'
+        exit(err_msg)
+    if len(args.out) < len(args.bounds):
+        err_msg = 'There are more bounds than custom map names.'
+        exit(err_msg)
 
-        for map_name, bounds in zip(args.out, args.bounds):
-            downloads.append((map_name, tuple(bounds)))
+    for map_name, bounds in zip(args.out, args.bounds):
+        downloads.append((map_name, tuple(bounds)))
 
     main(downloads)
